c3fc6220a95749a6a1b45e77360346659ffc1335
Rob Kalajian <rkalajian@gmail.com>
2026-02-16 09:26:07 -0500

    Remove gitignored files from tracking, add .gitignore"

--- strapi-backend/dist/src/api/form-submission/routes/form-submission.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::form-submission.form-submission', {
-    config: {
-        create: { auth: false },
-    },
-});
