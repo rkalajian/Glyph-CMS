dc3eaa8df18646582405d74bcf0ad609f5e55ea6
Rob Kalajian <rkalajian@gmail.com>
2026-04-09 06:53:28 -0400

    Packages

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -5,8 +5,30 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   const mailgunDomain = env('MAILGUN_DOMAIN', '');
   const useMailgun = Boolean(mailgunKey && mailgunDomain);
 
+  const cloudinaryName = env('CLOUDINARY_NAME', '');
+  const cloudinaryKey = env('CLOUDINARY_KEY', '');
+  const cloudinarySecret = env('CLOUDINARY_SECRET', '');
+  const useCloudinary = Boolean(cloudinaryName && cloudinaryKey && cloudinarySecret);
+
   return {
     'strapi-cloud': { enabled: false },
+    upload: useCloudinary
+      ? {
+          config: {
+            provider: '@strapi/provider-upload-cloudinary',
+            providerOptions: {
+              cloud_name: cloudinaryName,
+              api_key: cloudinaryKey,
+              api_secret: cloudinarySecret,
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          },
+        }
+      : {},
     email: useMailgun
       ? {
           config: {
